fc5944d49045b7006597e55d79f3ded281bca8a8
Михаил Денисов <Migelius.invest@gmail.com>
2023-02-28 13:51:05 +0300

    re

--- unificationOfTroops/Program.cs
+++ unificationOfTroops/Program.cs
@@ -17,10 +17,10 @@ namespace unificationOfTroops
                 "Усачев",
                 "Купер"
             };
-            
+
             List<string> soldiers2 = new List<string>
             {
-                "Родионов",
+                "Зуйбалов",
                 "Островерх",
                 "Усманов",
                 "Колесников",
@@ -28,9 +28,29 @@ namespace unificationOfTroops
                 "Нектаров"
             };
 
+            ShowSoldiers(soldiers1);
+            ShowSoldiers(soldiers2);
+
             var filteredSoldiers = soldiers1.Where(soldier => soldier.StartsWith("Б")).ToList();
             var unitedSoldiers = soldiers2.Union(filteredSoldiers).ToList();
             soldiers2 = unitedSoldiers;
+
+            var tempSoldiers = soldiers1.Except(soldiers2).ToList();
+            soldiers1 = tempSoldiers;
+
+            ShowSoldiers(soldiers1);
+            ShowSoldiers(soldiers2);
+            Console.ReadLine();
+        }
+
+        static void ShowSoldiers(List<string> soldiers)
+        {
+            foreach (var soldier in soldiers) 
+            {
+                Console.WriteLine(soldier);
+            }
+
+            Console.WriteLine();
         }
     }
 }
